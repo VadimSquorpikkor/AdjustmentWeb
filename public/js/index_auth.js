e44321147fedff24c4a59528253da335fabe4177
VadimSquorpikkor <uniqueehub1024>
2022-02-09 16:40:31 +0300

    1.44 - 09/02/22 1. По-мелочи

--- public/js/index_auth.js
+++ public/js/index_auth.js
@@ -43,6 +43,28 @@ document.getElementById("sign_out").onclick = function () {
     });
 };
 
+/*firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+        /!** @type {firebase.auth.OAuthCredential} *!/
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+});*/
+
 /**Слушает состояние авторизации и выводит в консоль статус. Включает/отключает отображение элементов UI*/
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
