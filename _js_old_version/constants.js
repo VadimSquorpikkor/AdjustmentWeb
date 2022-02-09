b31b1d4ebffeede6d2012b442355a012d06577db
VadimSquorpikkor <uniqueehub1024>
2022-02-09 17:16:05 +0300

    1.44 - 09/02/22 1. Подготовка к глобальной переделке

---
+++ _js_old_version/constants.js
@@ -0,0 +1,72 @@
+/**Имя базы данных*/
+console.log('...загрузка constant');
+const DBASE = firebase.firestore();
+const TABLE_NAMES = "names";
+const TABLE_SETTINGS = "_settings";
+const SECURE_KEY_DOC = "secure_key";
+
+const ALL_DEVICES = "Все устройства";
+const ALL_STATES = "Все статусы";
+const ALL_LOCATIONS = "Все локации";
+const ALL_EMPLOYEES = "Все сотрудники";
+const REPAIR_UNIT = "Ремонт";
+const FOUND_NOTHING = "Ничего не найдено";
+
+//Новые const для новой БД:
+const TABLE_UNITS = "units";
+const UNIT_DESCRIPTION = "description";
+const UNIT_DEVICE = "device_id";
+const UNIT_EMPLOYEE = "employee_id";
+const UNIT_ID = "id";
+const UNIT_INNER_SERIAL = "inner_serial";
+const UNIT_LOCATION = "location_id";
+const UNIT_SERIAL = "serial";
+const UNIT_STATE = "state_id";
+const UNIT_TYPE = "type_id";
+
+const TABLE_STATES = "states"; //в прошлом profile
+const STATE_LOCATION = "location_id";
+const STATE_NAME = "name";
+const STATE_TYPE = "type_id";
+
+const TABLE_EVENTS = "events"; //в прошлом states
+const EVENT_DATE = "date";
+const EVENT_DESCRIPTION = "description";
+const EVENT_LOCATION = "location_id";
+const EVENT_STATE = "state_id";
+const EVENT_UNIT = "unit_id";
+
+const TABLE_EMPLOYEES = "employees"; //в прошлом users
+const EMPLOYEE_EMAIL = "email"; //email нельзя использовать в качестве id, так как у пользователя может поменяться email, и тогда при необходимости выбрать устройства пользователя нужно будет искать и по старому email и по новому
+const EMPLOYEE_ID = "id";
+const EMPLOYEE_LOCATION = "location_id";
+const EMPLOYEE_NAME = "name";
+
+const TABLE_LOCATIONS = "locations";
+const LOCATION_ID = "id";
+const LOCATION_NAME = "name";
+
+const TABLE_DEVICES = "devices";
+const DEVICE_ID = "id";
+const DEVICE_NAME = "name";
+const DEVICE_TYPE = "type";
+
+const TABLE_DEVSETS = "device_set";
+
+const TYPE_ANY = "any_type";
+const TYPE_REPAIR = "repair_type";
+const TYPE_SERIAL = "serial_type";
+
+const SERIAL_TYPE = "serial_type";
+const REPAIR_TYPE = "repair_type";
+
+const ANY_VALUE = "any_value";
+const STATE_PREF = "state_";
+const DESCENDING = "desc";
+const EMPTY_VALUE = "- - -";
+
+const ASSEMBLY_LOCATION_ID = "assembly";
+const ADJUSTMENT_LOCATION_ID = "adjustment";
+const GRADUATION_LOCATION_ID = "graduation";
+const REPAIR_AREA_LOCATION_ID = "repair_area";
+const SOLDERING_LOCATION_ID = "soldering";
---
+++ _js_old_version/for_qr.js
@@ -0,0 +1,56 @@
+/**НЕ ИСПОЛЬЗУЕТСЯ*/
+
+
+function insertSpinnerByArray(name, arr) {
+    if (document.getElementById(name) != null) {
+        let code = '';
+        for (let i = 0; i < arr.length; i++) {
+            code += '<option value=' + (i + 1) + '>' + arr[i] + '</option>' //через цикл добавляется строка спиннера (option) вида: <option value="1">БДКГ-02</option>
+        }
+        document.getElementById(name).innerHTML = '   <select>' + code + '</select>'; //добавляем открывающий и закрывающий тэг и выводим всё в элемент по id
+    }
+}
+
+function insertDevNames(/*arr_name, */arr_id) {
+    // deviceNameList = arr_name;
+    deviceIdList = arr_id;
+
+    let arr2 = [];
+    for (let i = 0; i < arr_id.length; i++) {
+        arr2.push(arr_id[i]);
+    }
+
+    arr2.unshift(REPAIR_UNIT);//для selected_type_for_generator в начало списка добавляю 'Ремонт'
+    insertSpinnerByArray('selected_type_for_generator', arr2);
+}
+
+let deviceIdList = [];
+let deviceNameList = [];
+
+function listen(database, table, func) {
+    database.collection(table)
+        .onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+                // console.log(change.doc.data());
+                getPairedCollectionFromDB(table, function (arr_id/*, arr_name*/) {
+                    func(/*arr_name, */arr_id); //Эта функция
+                });
+            });
+        });
+}
+
+function getPairedCollectionFromDB(table, func) {
+    let arr_id = [];
+    // let arr_name = [];
+    DBASE.collection(table)
+        .get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            arr_id.push(doc.data().id);
+            // arr_name.push(doc.data().name_ru);
+        });
+        func(arr_id/*, arr_name*/);
+    });
+}
+
+/**Лисенер для списка имен устройств*/
+listen(DBASE, TABLE_DEVICES, insertDevNames);
